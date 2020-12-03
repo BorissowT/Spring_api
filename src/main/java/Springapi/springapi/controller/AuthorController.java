2e5f7e24df9243a12099229d223b3ae316faf3cb
BorissowT <tbs999borisov@gmail.com>
2020-12-03 20:14:39 +0300

    add kafka topic with 2 partitions

--- src/main/java/Springapi/springapi/controller/AuthorController.java
+++ src/main/java/Springapi/springapi/controller/AuthorController.java
@@ -1,13 +1,13 @@
 package Springapi.springapi.controller;
 
 import Springapi.springapi.entity.AuthorModel;
-import Springapi.springapi.entity.ClientModel;
 import Springapi.springapi.exception.ResourceNotFoundException;
 import Springapi.springapi.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -20,6 +20,10 @@ import java.util.Map;
 public class AuthorController {
 
     @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "TEST1";
+
+    @Autowired
     private AuthorRepository authorRepository;
 
     @GetMapping("/authors")
@@ -30,10 +34,11 @@ public class AuthorController {
     @GetMapping("/authors/{id}")
     public ResponseEntity getAuthorById(@PathVariable(value = "id") Long authorId)
             throws ResourceNotFoundException {
+        
         AuthorModel author =
                 authorRepository
                         .findById(authorId)
-                        .orElseThrow(() -> new ResourceNotFoundException("Author not found on :: " + authorId));
+                        .orElseThrow(() -> new ResourceNotFoundException("Author not found on :: " + authorId));             
         return ResponseEntity.ok().body(author);
     }
 
@@ -43,6 +48,8 @@ public class AuthorController {
         if (principal == null) {
             throw new ForbiddenException();
         }
+
+        sendMessage("created autor: " + author.getFNAME() + " " + author.getLNAME());
         return authorRepository.save(author);
     }
 
@@ -62,4 +69,9 @@ public class AuthorController {
         response.put("deleted", Boolean.TRUE);
         return response;
     }
+
+    public void sendMessage(String msg) {
+        kafkaTemplate.send(TOPIC, msg);
+    }
+
 }
